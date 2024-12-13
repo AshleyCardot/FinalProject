cb3c4e8a2088086747736833b4a1b19d236a3816
AshleyCardot <acardot@smu.edu>
2024-12-13 15:45:10 -0600

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Ashley Cardot on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
